399b506e341217bb901138799c874c76415e2f3e
skulluglify <skulluglify@outlook.com>
2023-06-02 22:47:06 +0700

    feat/update cors management

--- app/cors.go
+++ app/cors.go
@@ -14,9 +14,11 @@ func ManageControlResourceShared(pn papaya.NetImpl) error {
   }
 
   manageConsumers.Grant("http://localhost")
+  manageConsumers.Grant("http://localhost:5173") // vite - react
   manageConsumers.Grant("http://localhost:8000")
   manageConsumers.Grant("https://academy.skfw.net") // secure deploy
   manageConsumers.Grant("https://skfw.net")         // secure deploy
+  manageConsumers.Grant("https://frontend-react-git-staging-academade.vercel.app")
 
   pn.Use(cors.MakeMiddlewareForManageConsumers(manageConsumers))
 
